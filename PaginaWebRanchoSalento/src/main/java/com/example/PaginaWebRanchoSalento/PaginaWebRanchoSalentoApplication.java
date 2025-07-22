89e55fc352a0e2443c0fb729713d5ae8b2c7c508
Arias08426 <jarias1028@cue.edu.co>
2025-07-22 11:34:31 -0500

    Estructura inicial, arquitectura orientada a servicios para comenzar a realizar la pagina web RS

---
+++ PaginaWebRanchoSalento/src/main/java/com/example/PaginaWebRanchoSalento/PaginaWebRanchoSalentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.PaginaWebRanchoSalento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaWebRanchoSalentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaWebRanchoSalentoApplication.class, args);
+	}
+
+}
---
+++ PaginaWebRanchoSalento/src/test/java/com/example/PaginaWebRanchoSalento/PaginaWebRanchoSalentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PaginaWebRanchoSalento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaWebRanchoSalentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
