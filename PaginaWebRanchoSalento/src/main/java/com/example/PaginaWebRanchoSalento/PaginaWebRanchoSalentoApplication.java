cb4588a28d4a2358b4c88fac37e3d12477416d73
Arias08426 <jarias1028@cue.edu.co>
2025-09-09 07:48:39 -0500

    Configuracion inicial del proyecto, actualizada

--- PaginaWebRanchoSalento/src/main/java/com/example/PaginaWebRanchoSalento/PaginaWebRanchoSalentoApplication.java
+++ PaginaWebRanchoSalento/src/main/java/com/example/PaginaWebRanchoSalento/PaginaWebRanchoSalentoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PaginaWebRanchoSalentoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PaginaWebRanchoSalentoApplication.class, args);
 	}
-
 }
