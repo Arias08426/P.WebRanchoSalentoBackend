51200ce6bc8838a3669e0fc001e155b156212db5
Arias08426 <jarias1028@cue.edu.co>
2025-09-09 07:46:03 -0500

    Excepciones es para manejar errores de forma centralizada y clara en la aplicación

---
+++ PaginaWebRanchoSalento/src/main/java/com/example/PaginaWebRanchoSalento/exception/OurException.java
@@ -0,0 +1,8 @@
+package com.example.PaginaWebRanchoSalento.exception;
+
+public class OurException extends RuntimeException {
+
+    public OurException(String message) {
+        super(message);
+    }
+}
